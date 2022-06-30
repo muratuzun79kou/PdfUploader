dd857472147631ebe11fe1fea23af1732a440bd8
Zeynep Asli Erhan <zeynepaslierhan@gmail.com>
2022-06-30 10:27:16 +0300

    proje

---
+++ PdfUploeder/PdfUploeder/Controllers/Files1Controller.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PdfUploeder.Models;
+using System.IO;
+
+namespace PdfUploeder.Controllers
+{
+    public class Files1Controller : Controller
+    {
+        IHostingEnvironment _hostingEnvironment = null;
+        public Files1Controller(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [HttpGet]
+        public IActionResult Index(string fileName="")
+        {
+            FileClass fileObj = new FileClass();
+            fileObj.Name=fileName;
+            string path = $"{_hostingEnvironment.WebRootPath}\\files\\";
+            int nId = 1;
+            foreach (string pdfPath in Directory.EnumerateFiles(path,"*.pdf"))
+            {
+                fileObj.Files.Add(new FileClass()
+                {
+                    FileId =nId++,
+                    Name = Path.GetFileName(pdfPath),
+                    Path = pdfPath
+                });
+            }
+
+            return View(fileObj);
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormFile file, [FromServices] IHostingEnvironment hostingEnvironment)
+        {
+            string fileName = $"{hostingEnvironment.WebRootPath}\\files\\{file.FileName}";
+            using (FileStream fileStream = System.IO.File.Create(fileName))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+            }
+            return Index();
+        }
+    }
+}
---
+++ PdfUploeder/PdfUploeder/Models/FileClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PdfUploeder.Models
+{
+    public class FileClass
+    {
+        [Key]
+        public int FileId { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public string Path { get; set; } = "";
+        public List<FileClass> Files { get; set; } = new List<FileClass>();
+    }
+}
